4b43a4b1d76a772598234960fa57770d9cea383b
hitha-shetty <hitharaghava@gmail.com>
2025-07-14 04:40:45 +0530

    installed google maps api + api key

--- src/pages/SingleOne.jsx
+++ src/pages/SingleOne.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router';
 import axios from 'axios';
 import '../Styles/Single.css';
